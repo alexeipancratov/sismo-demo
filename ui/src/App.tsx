1f32f0320caed350ccba6718eda72a1c2e5748b1
Alexei Pancratov <drivergear@gmail.com>
2023-07-02 14:59:21 -0400

    Initial

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AuthType, SismoConnectButton } from '@sismo-core/sismo-connect-react';
+import { config } from './sismo-connect-config';
 
 function App() {
   return (
@@ -10,6 +12,14 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <SismoConnectButton
+          config={config}
+          // request proof of Github ownership
+          auths={[{ authType: AuthType.VAULT }]}
+          onResponseBytes={(response: string) => {
+            // call your contract with the response as bytes
+          }}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ ui/src/sismo-connect-config.ts
@@ -0,0 +1,5 @@
+import { SismoConnectConfig } from "@sismo-core/sismo-connect-react";
+
+export const config: SismoConnectConfig = {
+  appId: "0x0e27dc7006cb9b360cfe9297005d2e71",
+};
